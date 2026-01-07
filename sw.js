fe373e60db61cb2312e660b6086830cdab6b355e
yousefalhammam-cpu <yousef.alhammam@gmail.com>
2026-01-07 22:06:15 +0300

    Update service worker cache version to v5

--- sw.js
+++ sw.js
@@ -1,29 +1,32 @@
-const CACHE = "ashrae170p-pwa-v2";
+const CACHE = "ashrae170p-pwa-v5";
+
 const ASSETS = [
   "./",
   "./index.html",
   "./app.js",
   "./data.json",
   "./manifest.webmanifest",
+  "./sw.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c)=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => (k===CACHE)?null:caches.delete(k))))
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
   const req = e.request;
   e.respondWith(
     caches.match(req).then((cached) => cached || fetch(req).then((resp)=>{
-      // Runtime cache for same-origin
       const copy = resp.clone();
       caches.open(CACHE).then(c=>c.put(req, copy)).catch(()=>{});
       return resp;
