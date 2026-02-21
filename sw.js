f6013f5cefa958ec88cabe133afd2fc5c5020e08
yousefalhammam-cpu <yousef.alhammam@gmail.com>
2026-02-21 19:00:49 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,8 @@ const cacheName = 'aircalc-pro-v3';
 const assets = [
   './',
   './index.html',
-  './styles.css',
   './app.js',
-  './data.json',
+  './styles.css',
   './manifest.webmanifest'
 ];
 
@@ -17,13 +16,13 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)))
+      Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))
     )
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
