659309de32b396aef49c72f2602e737bc872d289
yousefalhammam-cpu <yousef.alhammam@gmail.com>
2026-02-22 04:01:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,46 @@
-/* Air Calc Pro - Service Worker for Offline Access */
-const cacheName = 'aircalc-pro-v3';
-const assets = [
-  './',
-  './index.html',
-  './app.js',
-  './styles.css',
-  './data.json',
-  './manifest.webmanifest'
+/* Air Calc Pro - Service Worker */
+const CACHE_NAME = "aircalc-pro-v4";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./data.json",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-// Install
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-// Activate
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)))
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
-// Fetch
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then(cached => {
+    caches.match(event.request).then((cached) => {
       if (cached) return cached;
 
       return fetch(event.request)
-        .then(res => {
-          // اختياري: نخزن الملفات المحلية فقط
-          const url = new URL(event.request.url);
-          if (url.origin === location.origin) {
-            const resClone = res.clone();
-            caches.open(cacheName).then(cache => cache.put(event.request, resClone));
-          }
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
           return res;
         })
-        .catch(() => caches.match('./index.html'));
+        .catch(() => caches.match("./index.html"));
     })
   );
 });
