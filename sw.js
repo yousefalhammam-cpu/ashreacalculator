69e5aad4e3d5aff958636d8d54bf4a1645db3e0b
yousefalhammam-cpu <yousef.alhammam@gmail.com>
2026-02-22 23:41:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aircalc-pro-v3';
+const CACHE_NAME = 'aircalc-pro-cache-v1';
 const ASSETS = [
   './',
   './index.html',
@@ -17,25 +17,15 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-
-      return fetch(event.request).then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return res;
-      }).catch(() => caches.match('./index.html'));
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
