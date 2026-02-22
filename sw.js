9e25d1eca4ba80769a22c0eb830a859f3176eae4
yousefalhammam-cpu <yousef.alhammam@gmail.com>
2026-02-22 16:08:21 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-/* Air Calc Pro - Service Worker */
 const CACHE_NAME = 'aircalc-pro-v3';
 const ASSETS = [
   './',
@@ -10,22 +9,20 @@ const ASSETS = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
     )
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
