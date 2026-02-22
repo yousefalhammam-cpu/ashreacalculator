8a0db78e91fdfc589d61e595bfdfa8b425727e08
yousefalhammam-cpu <yousef.alhammam@gmail.com>
2026-02-23 01:53:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,33 @@
-const cacheName = 'aircalc-pro-v3';
-const assets = [
-  './',
-  './index.html',
-  './app.js',
-  './styles.css',
-  './manifest.webmanifest'
+/* Air Calc Pro - Service Worker (Offline) */
+const CACHE_NAME = "aircalc-pro-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
